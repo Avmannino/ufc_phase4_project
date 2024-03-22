c8f9ab7654700512e21be7aade64e1d5e2563949
NickRayman <nlhrayman@gmail.com>
2024-03-22 18:30:59 -0400

    more style changes

--- src/FighterData.js
+++ src/FighterData.js
@@ -1,12 +1,17 @@
 
 export default function FighterData({fighter,handleBackButton}){
     return(
-        <div style={{color:"white"}}>
+        <>
+        <div className="fighter_data">
             <input type="submit" className="backButton" value="BACK" onClick={(event)=>handleBackButton(event)} />
             <p>{fighter.name}</p>
             <p>{fighter.nickname?'"'+fighter.nickname+'"':null}</p>
             <p>{fighter.wins}-{fighter.losses}-{fighter.draws}</p>
+            
+        </div>
+        <div className="fighter_data">
             <p>CHARTS HERE</p>
         </div>
+        </>
     )
 }
