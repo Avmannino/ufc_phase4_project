6e9ca9cb9079147462cee1eb897563f89415ba00
NickRayman <nlhrayman@gmail.com>
2024-03-22 18:26:29 -0400

    style updated

--- src/FighterInfo.js
+++ src/FighterInfo.js
@@ -4,7 +4,7 @@ export default function FighterInfo({fighter,onFighterClick}){
 
     return(
         <div style={{textAlign:"center",strokeWidth:"2px",border: "2px solid white",maxWidth:"500px",marginLeft:"700px"}} onClick={()=>onFighterClick(fighter)}>
-            <p style={{color:"white"}} className='fighterLink'>{fighter.name}</p>
+            <p className='fighterLink'>{fighter.name}</p>
         </div>
     )
 }
