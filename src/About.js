e2021cf0c2f673cbb0393aa17fb8248b3c1b7156
Adam Mannino <amannino92@gmail.com>
2024-03-27 23:27:14 -0400

    formatting and theme for map & about section

--- src/About.js
+++ src/About.js
@@ -6,11 +6,11 @@ function About() {
         <div className="container">
             <p>
                 <h3>About this App</h3>
-                UFC FIGHT MAPS is an interactive dashboard based on the highest level of mixed martial arts, the UFC or Ultimate Fighting Championship. Its principal aim is to provide an overview of past events and fight-cards, whilst allowing the user to probe the data and explore various trends on a per-Fighter basis, right down to individual strike percentage.
+                UFC DATABASE is an interactive dashboard and data visualizer the UFC or Ultimate Fighting Championship. Its principal aim is to provide an overview of past events and fight-cards, while also allowing the user to probe the data and explore various trends on a per-Fighter basis, right down to individual strike percentage.
             </p>
             <p>
                 <h4>About The Data Source</h4>
-                Certain data was web-scraped via a third-party API and manually entered into a master JSON file. Certain packages we used did not allow for the use of parsing data via a backend server such as FlaskSql. We did, however, manage to obtain additional datasets for the more granular fight and fighter data. This was entered into a Flask db and is routed into our frontend React components.
+                Certain data was web-scraped via a third-party API and manually entered into a master JSON file. The library for our Event Map did not allow for the utilization of data via a backend database or API. We plan to implement this data into our backend database in the near future. We did however manage to obtain additional datasets for the more granular fight and fighter values.
             </p>
             <p>
                 <h4>Motivation</h4>
@@ -22,6 +22,14 @@ function About() {
             </p>
             <div className='contact'>
                 <h4 className='developers'>Developed By:</h4>
+                <p>
+                        <h5 className='disclaimer'>Disclaimer:</h5> 
+                        All product and company names are trademarks™ or registered® trademarks of the UFC™. Use of them does not imply any affiliation with or endorsement by them.
+
+                        Any product names, logos, brands and other trademarksor images featured or referred to within this website are the property of the UFC™, its entities and its subsidiaries.
+
+                        This website is for educational purposes only. No copyright infringement is intended.
+                </p>
                 <ul>
                     <li>
                         <strong>Adam Mannino</strong>
